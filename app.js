1a4846805bcd2729d05cd44dd9fe9bcb561ae79a
Wine635 <winewine1623@gmail.com>
2020-03-02 19:42:56 +0630

    change style

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 let form=document.querySelector("form");
 let button=document.querySelector("#button");
-let div1=document.querySelector('#name1');
-let div2=document.querySelector('#email1');
-let div3=document.querySelector('#pass1');
+//let div1=document.querySelector('#name1');
+//let div2=document.querySelector('#email1');
+//let div3=document.querySelector('#pass1');
 
 let ch=/(^[^0-9])+([a-zA-Z0-9]{10,15})(\@[a-z]{5})\.([a-z]{3})$/i;
 let chpass=/^[0-9a-zA-Z]{8,10}/i;
